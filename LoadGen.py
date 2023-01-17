a63040e5958d0cfb9d905b2ea8265f04bb8cd34f
Younbin LIM <105615106+YB-LIM@users.noreply.github.com>
2023-01-17 09:08:29 +0900

    Update LoadGen.py

--- LoadGen.py
+++ LoadGen.py
@@ -4,7 +4,7 @@ This python script generates VDLOAD subroutine for
 Gaussian random impact on periodic cell
 
 AUTHOR
-Name: Yougbin LIM
+Name: Youngbin LIM
 E-mail: lyb0684@naver.com
 
 Written on Intel® Fortran Version 19.0.5 and python 3.9
